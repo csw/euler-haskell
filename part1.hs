4a601a28c563c3926a0ac239c8c4b895d1c95a39
Clayton Wheeler <cswh@umich.edu>
2013-05-22 19:47:47 -0400

    Problem 2 done.

---
+++ part1.hs
@@ -0,0 +1,23 @@
+p1 = foldl (+) 0 [x | x <- [1..999], x `mod` 3 == 0 || x `mod` 5 == 0]
+
+-- Sum of a finite arithmetic series
+-- n elements, from a1 to an
+p1_arithSum :: (Integral a) => a -> a -> a -> a
+p1_arithSum n a1 an = (n * (a1 + an)) `div` 2
+
+p1_arithSumTo :: (Integral a) => a -> a -> a
+p1_arithSumTo n max =
+  let maxMult = max `div` n
+  in p1_arithSum maxMult n (n * maxMult)
+
+p1a :: Integral a => a
+p1a = (p1_arithSumTo 3 999) + (p1_arithSumTo 5 999) - (p1_arithSumTo 15 999)
+
+p2_fib :: Int -> Int -> [Int]
+p2_fib a b = a : p2_fib b (a + b)
+
+p2_fib_seq :: [Int]
+p2_fib_seq = p2_fib 1 2
+
+p2 :: Int
+p2 = foldl (+) 0 [n | n <- takeWhile (< 4000000) p2_fib_seq, even n]
