d1046f51f27d5c3cb902ba093f16872a7cd3c8c9
Clayton Wheeler <cswh@umich.edu>
2013-05-22 20:35:43 -0400

    Did P3.

--- part1.hs
+++ part1.hs
@@ -1,3 +1,6 @@
+import Data.Set (Set)
+import qualified Data.Set as Set
+
 -- P1: Sum of all multiple of 3 or 5 below 1000.
 
 p1 = foldl (+) 0 [x | x <- [1..999], x `mod` 3 == 0 || x `mod` 5 == 0]
@@ -32,3 +35,27 @@ p2_fib a b = a : p2_fib b (a + b)
 p2_fib_seq :: [Int]
 p2_fib_seq = p2_fib 1 2
 
+
+-- P3: What is the largest prime factor of the number 600851475143?
+
+-- find primes up to sqrt(N) with the Sieve of Eratosthenes
+-- test each prime to see if it evenly divides N
+
+-- Slow! But it works.
+p3 :: Int -> Int
+p3 n =
+  let primes = p3_sieve_e (fromIntegral $ floor $ sqrt $ fromIntegral n)
+      factors = [p | p <- primes, n `mod` p == 0]
+  in last factors
+
+p3_sieve_e' :: Int -> Int -> Set Int -> [Int]
+p3_sieve_e' n max nonPrimes
+  | n >= max               = []
+  | Set.member n nonPrimes = p3_sieve_e' (n+2) max nonPrimes
+  | otherwise              =
+    let multiples = Set.fromList $ takeWhile (<= max) $ iterate (+n) n
+        newSet = Set.union nonPrimes multiples
+      in n : p3_sieve_e' (n+2) max newSet
+
+p3_sieve_e     :: Int -> [Int]
+p3_sieve_e max = 2 : p3_sieve_e' 3 max (Set.singleton 2)
