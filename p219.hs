2f2b222855d189367aebb9f1a8d668f3ce9df9aa
Clayton Wheeler <cswh@umich.edu>
2013-07-01 00:52:18 -0400

    Use Integer for arbitrary precision.

--- p219.hs
+++ p219.hs
@@ -1,23 +1,23 @@
 import System.Environment
 
-type Freq = Int
+type Freq = Integer
 
-data FStep = Step Int Int Freq Freq Freq Freq
+data FStep = Step Integer Integer Freq Freq Freq Freq
              deriving Show
 
 seqStart :: FStep
 seqStart = Step 2 1 1 0 0 1
 
-minCost :: FStep -> Int
+minCost :: FStep -> Integer
 minCost (Step c base f0 f1 f2 f3) =
   base*f0 + (base+1)*f1 + (base+2)*f2 + (base+3)*f3
 
-findMinCost :: Int -> Int
+findMinCost :: Integer -> Integer
 findMinCost size = (minCost st) + delta*(base+5)
   where st@(Step c base f0 _ _ _) = lastFreqStepFor size seqStart
         delta = size - c
 
-lastFreqStepFor :: Int -> FStep -> FStep
+lastFreqStepFor :: Integer -> FStep -> FStep
 lastFreqStepFor target st@(Step c _ f0 _ _ _)
   | (c+f0) > target = st
   | otherwise       = lastFreqStepFor target $ freqStep st
