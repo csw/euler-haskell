ac6d2c0ac580b54fe4368ad58a0305c5633ee1e3
Clayton Wheeler <cswh@umich.edu>
2013-07-20 14:11:27 -0400

    Naive, slow solution to #114. Need to work on this for #115.

---
+++ p114.hs
@@ -0,0 +1,19 @@
+-- naive way
+
+placements :: Int -> Int
+placements w = sum [placeN n w | n <- [0..maxN]]
+  where maxN = (w+1) `div` 4
+
+placeN :: Int -> Int -> Int
+placeN 0 _ = 1
+placeN 1 w
+  | w < 3     = 0
+  | otherwise = (w-2) + placeN 1 (w-1)
+placeN n w
+  | w < (4*n)-1 = 0
+  | otherwise   = placeRest + placeN n (w-1)
+  where placeRest = sum [placeN (n-1) w' | w' <- [(4*(n-1)-1)..(w-4)]]
+
+main = do
+  putStrLn $ show $ placements 50
+
